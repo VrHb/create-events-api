6d874123c1863929757dc754fe115402618d1592
Viktor Abdukarimov <viredmi@gmail.com>
2024-03-20 16:13:31 +0300

    Init django project

---
+++ README.md
@@ -0,0 +1,50 @@
+# Сервис для создания мероприятий
+
+## Как запустить
+
+### Переменные окружения:
+
+Создайте файл `.env`:
+
+```
+touch .env
+```
+
+1. Секретный ключ проекта:
+
+```
+echo "DJANGO_SECRET_KEY=<ключ проекта>" >> .env
+```
+
+2. Название директории для статики
+
+```sh
+echo "STATIC_DIR_NAME=<название директории>"
+```
+
+### Установка c помощью docker
+
+```sh
+docker-compose build && docker-compose up -d
+```
+
+### Установка вручную
+
+#### Выполните миграции в БД
+
+```sh
+python manage.py migrate
+```
+
+#### Установите бд REDIS
+
+```sh
+sudo apt install redis
+```
+
+#### Запустите сервер
+
+```sh
+python manage.py runserver
+```
+
---
+++ event_api/settings.py
@@ -0,0 +1,109 @@
+import os
+
+from pathlib import Path
+
+from environs import Env
+
+env = Env()
+env.read_env()
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = env.str('DJANGO_SECRET_KEY')
+
+DEBUG = env.bool('DEBUG', False)
+
+ALLOWED_HOSTS = env.list('HOSTS', ['127.0.0.1', '0.0.0.0'])
+
+INTERNAL_IPS = env.list('INTERNAL_IPS', ['127.0.0.1', '0.0.0.0'])
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+
+    'event_app'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'event_api.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'event_api.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'Europe/Moscow'
+
+USE_L10N = False 
+
+DATETIME_FORMAT="%Y-%m-%d%H:%M:%S"
+
+STATIC_URL = f'/{env.str("STATIC_DIR_NAME")}/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_CHARSET = 'utf-8'
